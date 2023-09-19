f4acef534781b2e294a5461170a0b1003cbaccb5
Maria Jose <mariaajose17@gmail.com>
2023-09-18 19:07:19 -0500

    feat: add normalize css and quote component

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import { ImQuotesLeft, ImQuotesRight } from "react-icons/im";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [quote, setQuote] =
+    useState(`¿Por qué lo usamos? Es un hecho establecido hace demasiado tiempo que
+    un lector se distraerá con el contenido del texto de un sitio mientras
+    que mira su diseño.`);
+
+  useEffect(() => {
+    if (quote.length > 140) {
+      setQuote(`${quote.slice(0, 137)}...`);
+    }
+  }, [quote]);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+      <div id="quote-box" className="container">
+        <p id="text">
+          <ImQuotesLeft />
+          {quote}
+          <ImQuotesRight />
         </p>
+        <p id="author">Maria Jose</p>
+        <button id="new-quote"></button>
+        <a
+          id="tweet-quote"
+          href="https://twitter.com/intent/tweet"
+          target="_blank"
+          rel="noreferrer"
+        ></a>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
