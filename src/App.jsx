5c3a2d15a86b955665805328f68ff2ddee902e45
Maria Jose <mariabecerrab17@gmail.com>
2024-06-06 10:17:11 -0500

    fix: :bug: arrangement color palette

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import { useEffect, useState } from "react";
 import { ImQuotesLeft, ImQuotesRight } from "react-icons/im";
-import { FaXTwitter } from "react-icons/fa6";
-import { TiSocialTumbler } from "react-icons/ti";
 import "./App.css";
 
 function App() {
@@ -11,6 +9,7 @@ function App() {
     que mira su diseño.`);
 
   const [author, setAuthor] = useState("Maria José");
+  const [backgroundColor, setBackgroundColor] = useState("rgba(255,255,255,");
 
   const fetchQuote = () => {
     fetch("https://api.quotable.io/random")
@@ -20,6 +19,7 @@ function App() {
       .then((data) => {
         setQuote(data.content);
         setAuthor(data.author);
+        setBackgroundColor(generateRandomColor());
       })
       .catch((error) => {
         console.log(error);
@@ -38,9 +38,27 @@ function App() {
     }
   }, [quote]);
 
+  const generateRandomColor = () => {
+    const randomColor = `rgb(${200 + Math.floor(Math.random() * 56)}, ${
+      200 + Math.floor(Math.random() * 56)
+    }, ${200 + Math.floor(Math.random() * 56)})`;
+    document.documentElement.style.setProperty(
+      "--random-background-color",
+      randomColor
+    );
+    return randomColor;
+  };
+
   return (
     <>
-      <div style={{ width: "100%", display: "grid", placeContent: "center" }}>
+      <div
+        style={{
+          backgroundColor: backgroundColor,
+          width: "100%",
+          display: "grid",
+          placeContent: "center",
+        }}
+      >
         <div id="quote-box" className="container">
           <p id="text">
             <ImQuotesLeft style={{ marginRight: "0.5rem" }} />
@@ -49,29 +67,10 @@ function App() {
           </p>
           <p id="author">
             {" "}
-            {"— "}
+            {" — "}
             {author}
           </p>
           <div className="button-group">
-            <a
-              className="button"
-              id="tweet-quote"
-              href="https://twitter.com/intent/tweet"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <FaXTwitter />
-            </a>
-
-            <a
-              className="button"
-              id="tumblr-quote"
-              href="https://www.tumblr.com/login?redirect_to=https%3A%2F%2Fwww.tumblr.com%2Fwidgets%2Fshare%2Ftool%3Fposttype%3Dquote%26tags%3Dquotes%252Cfreecodecamp%26caption%3DBooker%2BT.%2BWashington%26content%3DFew%2Bthings%2Bcan%2Bhelp%2Ban%2Bindividual%2Bmore%2Bthan%2Bto%2Bplace%2Bresponsibility%2Bon%2Bhim%252C%2Band%2Bto%2Blet%2Bhim%2Bknow%2Bthat%2Byou%2Btrust%2Bhim.%26canonicalUrl%3Dhttps%253A%252F%252Fwww.tumblr.com%252Fbuttons%26shareSource%3Dtumblr_share_button"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <TiSocialTumbler />
-            </a>
             <button
               onClick={() => fetchQuote()}
               className="button"
